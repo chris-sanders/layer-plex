14b5f525b1403a3629ec5af64d5b41b2dc1d8346
Chris Sanders <sanders.chris@gmail.com>
2017-03-15 21:40:08 -0500

    Clean up old debs and reinstall if URL changes

--- reactive/plex.py
+++ reactive/plex.py
@@ -1,48 +1,46 @@
-from charms.reactive import when, when_not, set_state
+from charms.reactive import hook, when, when_any, when_not, set_state, remove_state
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core import hookenv 
 from charmhelpers.fetch import apt_install
 import urllib.request
-import os.path
+import os
 
-config = hookenv.config()
 
 @when_not('plex.installed')
 def install_plex():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
+  config = hookenv.config()
+  filepath = './debs'
+  try:
+    os.mkdir(filepath)
+  except OSError as e:
+    if e.errno is 17:
+      pass
 
-    filepath = './debs'
-    try:
-      os.mkdir(filepath)
-    except OSError as e:
-      if e.errno is 17:
-        pass
+  # Parse the filename from the URL
+  filename = config['download-url'].split('/')[-1]
+  
+  # Download the deb
+  fullpath = os.path.join(filepath,filename)
+  if not os.path.isfile(fullpath):
+    status_set('maintenance','downloading plex')
+    urllib.request.urlretrieve(config['download-url'],fullpath)
 
-    # Parse the filename from the URL
-    filename = config['download-url'].split('/')[-1]
-    
-    # Download the deb
-    fullpath = os.path.join(filepath,filename)
-    if not os.path.isfile(fullpath):
-    	status_set('maintenance','downloading plex')
-    	urllib.request.urlretrieve(config['download-url'],fullpath)
+  # Install package
+  status_set('maintenance','installing plex')
+  apt_install(fullpath)
 
-    # Install package
-    status_set('maintenance','installing plex')
-    apt_install(fullpath)
+  # Clean up debs
+  mtime = lambda f: os.stat(os.path.join(filepath,f)).st_mtime
+  sortedFiles = sorted(os.listdir(filepath),key=mtime)
+  deleteCount = max(len(sortedFiles)-config['keep-debs'],0)
+  for file in sortedFiles[0:deleteCount]:
+    os.remove(os.path.join(filepath,file))
 
-    # Clean up debs
-    # TODO check number of debs and clean up
+  hookenv.open_port(32400,'TCP')
+  set_state('plex.installed')
+  status_set('active','')
 
-    hookenv.open_port(32400,'TCP')
-    status_set('active','')
-    set_state('plex.installed')
+@when('config.changed.download-url')
+def url_updated():
+  install_plex()
+
